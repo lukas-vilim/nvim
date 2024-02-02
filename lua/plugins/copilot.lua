2a7aafb203e2c99f1dd9348d39708917d57439d0
lukas-vilim <lukas.vilim@gmail.com>
2024-02-02 17:50:25 +0100

    added copilot remaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,6 +6,13 @@ return {
 				expr = true,
 				replace_keycodes = false
 			})
+
+			--[[
+			vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+			vim.keymap.set('i', '<C-[>', '<Plug>(copilot-next)')
+			vim.keymap.set('i', '<C-]>', '<Plug>(copilot-previous)')
+			--]]
+
 			vim.g.copilot_no_tab_map = true
 		end
 	}
