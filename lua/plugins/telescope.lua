65a62c6ec353773e301514eb033f371f31d24520
lukas-vilim <lukas.vilim@gmail.com>
2024-09-24 16:52:51 +0200

    added telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,16 @@
 return {
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+			}
+		},
 		config = function()
-			require('telescope').setup {
+			local telescope = require('telescope')
+			telescope.setup {
 				defaults = {
 					file_ignore_patterns = {
 						"%.uasset",
@@ -24,8 +31,18 @@ return {
 						"%.manifest",
 						"%.locmeta",
 					}
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					}
 				}
 			}
+
+			telescope.load_extension('fzf')
 
 			local builtin = require('telescope.builtin')
 
